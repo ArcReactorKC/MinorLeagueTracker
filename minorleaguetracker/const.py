2b5e0eb0dd6d7e92c5128657c2dc63f16bf34c03
ArcReactorKC <125612580+ArcReactorKC@users.noreply.github.com>
2025-07-16 15:31:43 -0500

    Delete minorleaguetracker directory

--- minorleaguetracker/const.py
+++
@@ -1,8 +0,0 @@
-DOMAIN = "minorleaguetracker"
-
-API_KEY = "123"  # TheSportsDB free API key
-
-TEAM_SEARCH_API = f"https://www.thesportsdb.com/api/v1/json/{API_KEY}/searchteams.php?t="
-NEXT_EVENTS_API = f"https://www.thesportsdb.com/api/v1/json/{API_KEY}/eventsnext.php?id="
-LAST_EVENTS_API = f"https://www.thesportsdb.com/api/v1/json/{API_KEY}/eventslast.php?id="
-LOOKUP_TABLE_API = f"https://www.thesportsdb.com/api/v1/json/{API_KEY}/lookuptable.php?l={{league_id}}&s={{season}}"
